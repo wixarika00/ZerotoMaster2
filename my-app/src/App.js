58f50847f091046672870a1e2e74c613503dac9e
wixarika <agahydzik2@gmail.com>
2022-07-04 22:46:21 +0200

    commit search works

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -17,18 +17,22 @@ class App extends Component {
         }
     }
 
-    onSearchChange(event) {
-        console.log(event.target.value);  // event always needs target and value - we need to remember that
-        const filterRobots = this.state.robots.filter(robot =>{
-            return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+    onSearchChange = (event) => {  // any time you use your method on the component
+        this.setState({searchfield: event.target.value})
+        const filteredRobots = this.state.robots.filter(robot => {
+            return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
         })
+        console.log(filteredRobots); // event always needs target and value - we need to remember that
     }
     render()  {
+        const filteredRobots = this.state.robots.filter(robot => {
+            return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+        })
         return (
             <div className='tc'>
                 <h1>RoboFriends</h1>
                 <SearchBox searchChange={this.onSearchChange}/>
-                <CardList robots={this.state.robots}/>
+                <CardList robots={filteredRobots}/>
             </div>
         ); 
     }
